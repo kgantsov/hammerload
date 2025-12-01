284cf46f8578a0a9830ff58c5bb7f4b6c71dc1df
Kostiantyn Hantsov <k.gantsov@gmail.com>
2025-12-01 17:50:40 +0100

    Posibility to run benchmars concurrently emulating multiple users

---
+++ src/metrics/metrics.rs
@@ -0,0 +1,64 @@
+use hdrhistogram::Histogram;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+pub struct Metrics {
+    hist: Arc<Mutex<Histogram<u64>>>,
+    total_requests: Arc<Mutex<u64>>,
+    successful_requests: Arc<Mutex<u64>>,
+    failed_requests: Arc<Mutex<u64>>,
+}
+
+impl Metrics {
+    pub fn new() -> Self {
+        let hist = Histogram::<u64>::new(3).unwrap();
+        Self {
+            hist: Arc::new(Mutex::new(hist)),
+            total_requests: Arc::new(Mutex::new(0)),
+            successful_requests: Arc::new(Mutex::new(0)),
+            failed_requests: Arc::new(Mutex::new(0)),
+        }
+    }
+
+    pub async fn increment_total_requests(&self) {
+        let mut tr = self.total_requests.lock().await;
+        *tr += 1;
+    }
+
+    pub async fn increment_successful_requests(&self) {
+        let mut sr = self.successful_requests.lock().await;
+        *sr += 1;
+    }
+
+    pub async fn increment_failed_requests(&self) {
+        let mut fr = self.failed_requests.lock().await;
+        *fr += 1;
+    }
+
+    pub async fn total_requests(&self) -> u64 {
+        *self.total_requests.lock().await
+    }
+
+    pub async fn successful_requests(&self) -> u64 {
+        *self.successful_requests.lock().await
+    }
+
+    pub async fn failed_requests(&self) -> u64 {
+        *self.failed_requests.lock().await
+    }
+
+    pub async fn rps(&self, start_time: std::time::Instant) -> f64 {
+        let total = self.total_requests().await;
+        let elapsed = std::time::Instant::now().duration_since(start_time);
+        total as f64 / elapsed.as_millis() as f64 * 1000.0
+    }
+
+    pub async fn record_latency(&self, latency: u64) {
+        let mut hist = self.hist.lock().await;
+        hist.record(latency).unwrap();
+    }
+
+    pub async fn histogram(&self) -> Histogram<u64> {
+        self.hist.lock().await.clone()
+    }
+}
---
+++ src/scheduler/scheduler.rs
@@ -0,0 +1,117 @@
+use std::sync::Arc;
+
+use reqwest::Client;
+
+use crate::metrics::metrics::Metrics;
+
+pub struct Scheduler<'a> {
+    metrics: &'a Arc<Metrics>,
+    url: String,
+    concurrency: u64,
+    duration: u64,
+}
+
+impl<'a> Scheduler<'a> {
+    pub fn new(metrics: &'a Arc<Metrics>, url: String, concurrency: u64, duration: u64) -> Self {
+        Scheduler {
+            metrics,
+            url,
+            concurrency,
+            duration,
+        }
+    }
+
+    pub async fn run(&self) {
+        let start_bench = std::time::Instant::now();
+        let mut tasks = Vec::new();
+
+        let url = self.url.clone();
+
+        for _ in 0..self.concurrency {
+            let url = url.clone();
+            let duration = self.duration;
+            let metrics = Arc::clone(self.metrics);
+
+            tasks.push(tokio::spawn(async move {
+                Scheduler::run_client(&metrics, start_bench, url, duration).await;
+            }));
+        }
+
+        for task in tasks {
+            task.await.unwrap();
+        }
+
+        println!(
+            "Requests per second: {}",
+            self.metrics.rps(start_bench).await
+        );
+        println!(
+            "Successful requests: {}",
+            self.metrics.successful_requests().await
+        );
+        println!("Failed requests: {}", self.metrics.failed_requests().await);
+
+        println!(
+            "50th percentile: {}",
+            self.metrics.histogram().await.value_at_quantile(0.50)
+        );
+        println!(
+            "95th percentile: {}",
+            self.metrics.histogram().await.value_at_quantile(0.95)
+        );
+        println!(
+            "99th percentile: {}",
+            self.metrics.histogram().await.value_at_quantile(0.99)
+        );
+    }
+
+    async fn run_client(
+        metrics: &Arc<Metrics>,
+        start_bench: std::time::Instant,
+        url: String,
+        duration: u64,
+    ) {
+        let client = Client::new();
+
+        loop {
+            let result = Self::make_request(metrics, &client, &url).await;
+            Self::handle_request_result(metrics, result).await;
+
+            if std::time::Instant::now() >= start_bench + std::time::Duration::from_secs(duration) {
+                break;
+            }
+        }
+    }
+
+    async fn make_request(
+        metrics: &Arc<Metrics>,
+        client: &Client,
+        url: &str,
+    ) -> Result<String, reqwest::Error> {
+        let start = std::time::Instant::now();
+
+        let resp = client.get(url).send().await?;
+        let _status = resp.status();
+        let body = resp.text().await?;
+
+        let req_duration = start.elapsed();
+
+        metrics
+            .record_latency(req_duration.as_micros().try_into().unwrap_or(0))
+            .await;
+
+        Ok(body)
+    }
+
+    async fn handle_request_result(metrics: &Arc<Metrics>, result: Result<String, reqwest::Error>) {
+        match result {
+            Ok(_body) => {
+                metrics.increment_successful_requests().await;
+                metrics.increment_total_requests().await;
+            }
+            Err(_) => {
+                metrics.increment_failed_requests().await;
+            }
+        };
+    }
+}
